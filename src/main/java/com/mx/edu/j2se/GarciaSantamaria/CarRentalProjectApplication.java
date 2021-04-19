85b55c34c2633fb0c2885436454f2a9913e6ba78
Miguel Garcia Santamaria <mickeg53@gmail.com>
2021-04-18 21:26:10 -0500

    Database connection

--- src/main/java/com/mx/edu/j2se/GarciaSantamaria/CarRentalProjectApplication.java
+++ src/main/java/com/mx/edu/j2se/GarciaSantamaria/CarRentalProjectApplication.java
@@ -5,18 +5,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 @RestController
 public class CarRentalProjectApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CarRentalProjectApplication.class, args);
-	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalProjectApplication.class, args);
+
+		//Conection to database
+		String url = "jdbc:mysql://localhost:3306/carrentaldb";
+		String userName = "root";
+		String password = "123456789";
+
+
+		try {
+			Connection connection = DriverManager.getConnection(url, userName, password);
+			System.out.println("Connected to the database");
+
+		} catch (SQLException e) {
+			System.out.println("Database connection error !!");
+			e.printStackTrace();
+		}
+	}
 }
